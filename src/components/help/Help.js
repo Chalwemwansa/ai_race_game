fbebc9674a3c722a2e01295138eb03fc36516322
chalwe mwansa <chalwemwansa23@gmail.com>
2024-10-17 15:01:26 +0200

    new file:   src/assests/thumbnail.png 	new file:   src/assests/thumbnail2.png 	modified:   src/components/help/Help.js 	modified:   src/components/help/help.css
     Changes not staged for commit:
    	modified:   .gitignore

--- src/components/help/Help.js
+++ src/components/help/Help.js
@@ -11,11 +11,21 @@ export default function Help({ setHelp }) {
       <p className="helpText">
         The Maze is a simple game in which there is an automated AI that chooses
         its path in the game and follows it.
-        <b />
-        you move around the maze using the arrow keys on the keyboard
       </p>
       <p className="helpTexts">
-        you move around the maze using the arrow keys on the keyboard
+        you move around the maze using the arrow keys on the keyboard.
+      </p>
+      <p className="helpTexts">
+        The Start button starts a new game with different start or goal
+        positions.
+      </p>
+      <p className="helpTexts">
+        The Restart button starts a new game with the same start and goal
+        position.
+      </p>
+      <p className="helpTexts">
+        The Difficulty button toggles between easy and hard, changes the game to
+        hard or easy.
       </p>
     </div>
   );
